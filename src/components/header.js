63d8c5fa792c16496c53ba1d5ef34a9e7f292201
Junior Alves <jjalves96@gmail.com>
2020-01-28 10:33:48 -0300

    feat: base app

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
+  import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
@@ -12,7 +12,7 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+          maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
