a9378acdc80fa47fbf5d9a8f539d9266b21148b4
Junior Alves <jjalves96@gmail.com>
2020-02-04 11:11:49 -0300

    home screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Home</h1>
     <p>Bem-vido a Colmeia</p>
-    <p>Now go build something great.</p>
+    <p>Agencia de Marketing</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
